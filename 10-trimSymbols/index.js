03fd0bda062d3fafc3414ecfca549c451f7893a7
andrewNEW42 <sanjino56@gmail.com>
2021-09-09 19:51:09 +0300

    deploy of files

--- 10-trimSymbols/index.js
+++ 10-trimSymbols/index.js
@@ -5,6 +5,22 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-    const letterList = Array.from(string)
+    let letterList = Array.from(string)
 
+    let counter = 1
+    if(size === undefined) return string
+    if(string === "" || size === 0) return ""
+    const newList = letterList.filter((letter, index)=>{
+        if(index > 0 && letterList[index - 1] === letter){
+            counter = counter + 1
+        } else {
+            counter = 1
+        }
+        if(counter <= size){
+            return true
+        }else {
+            return false
+        }
+    })
+    return newList.join('')
 }
